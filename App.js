2fc0be0720665b0a7991fcdc91551e73dd04aaf5
anajuliif <anafeereira125@gmail.com>
2024-02-21 13:45:49 -0300

    react

--- App.js
+++ App.js
@@ -4,8 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text style={[styles.numero, {color:'purple'}]}>04</Text>
+      <Text style={[styles.sala, {backgroundColor:'pink'}]}>07</Text>
+      <Text style={[styles.nome, {color:'blue'}]}>Ana Julia Ferreira</Text>
+      <Text style={[styles.texto, {color:'red', backgroundColor:'blue'}]}>Senai Celso Charuri</Text>
     </View>
   );
 }
@@ -13,8 +15,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  nome: {
+   fontSize: 30, 
+   fontWeight: 'bold',
+   alignItems: 'center',
+   justifyContent: 'center',
+   left: '30%',
+  },
+  texto: {
+    fontSize: 20, 
+    fontWeight: 'bold',
+    alignItems: 'center',
+    justifyContent: 'center',
+    left: '30%',
+    borderBottomColor: 'red',
+    borderBottomWidth: 2,
+    marginBottom: 30,
+   },
+  numero: {
+    fontSize: 25, 
+    fontStyle: 'italic',
+    alignItems: 'center',
+    justifyContent: 'center',
+    right: '30%',
+    top: '25%',
+    borderBottomColor: 'black',
+    borderBottomWidth: 2,
+    marginBottom: 30,
+   },
+   sala:{
+   fontSize: 20,
+   }
 });
